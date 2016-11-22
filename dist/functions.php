c5742f71643d1341d2ab73d515c489c7887ef5b4
alaksandarjesus <alaksandarjesus@yahoo.co.in>
2016-11-23 02:16:47 +0530

    first commit

--- dist/functions.php
+++ dist/functions.php
@@ -97,26 +97,22 @@ function html5blank_header_scripts()
         if (HTML5_DEBUG) {
             // jQuery
             wp_deregister_script('jquery');
-            wp_register_script('jquery', get_template_directory_uri() . '/bower_components/jquery/dist/jquery.js', array(), '3.1.1');
+            wp_register_script('jquery', get_template_directory_uri() . '/bower_components/jquery/dist/jquery.js', array(), '1.11.1');
 
             // Conditionizr
-           // wp_register_script('conditionizr', get_template_directory_uri() . '/js/lib/conditionizr-4.3.0.min.js', array(), '4.3.0');
+            wp_register_script('conditionizr', get_template_directory_uri() . '/js/lib/conditionizr-4.3.0.min.js', array(), '4.3.0');
 
             // Modernizr
             wp_register_script('modernizr', get_template_directory_uri() . '/bower_components/modernizr/modernizr.js', array(), '2.8.3');
-
-            //materialize css
-            wp_register_script('materializejs', get_template_directory_uri() . '/bower_components/materialize/dist/js/materialize.min.js', array(), '0.97.8');
 
             // Custom scripts
             wp_register_script(
                 'html5blankscripts',
                 get_template_directory_uri() . '/js/scripts.js',
                 array(
-                    
-               //     'conditionizr',
+                    'conditionizr',
                     'modernizr',
-                    'jquery', 'materializejs'),
+                    'jquery'),
                 '1.0.0');
 
             // Enqueue Scripts
@@ -148,20 +144,15 @@ function html5blank_styles()
     if (HTML5_DEBUG) {
         // normalize-css
         wp_register_style('normalize', get_template_directory_uri() . '/bower_components/normalize.css/normalize.css', array(), '3.0.1');
-       
-        // materialize-css
-        wp_register_style('materialize', get_template_directory_uri() . '/bower_components/materialize/dist/css/materialize.css', array(), '0.97.8');
 
         // Custom CSS
-        wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array('normalize','materialize'), '1.0');
+        wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
 
         // Register CSS
         wp_enqueue_style('html5blank');
     } else {
-        // materialize-css
-        wp_register_style('materializemin', get_template_directory_uri() . '/bower_components/materialize/dist/css/materialize.min.css', array(), '0.97.8');
         // Custom CSS
-        wp_register_style('html5blankcssmin', get_template_directory_uri() . '/style.css', array('materializemin'), '1.0');
+        wp_register_style('html5blankcssmin', get_template_directory_uri() . '/style.css', array(), '1.0');
         // Register CSS
         wp_enqueue_style('html5blankcssmin');
     }
